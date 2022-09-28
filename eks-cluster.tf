d68318bb2669f1231655080916b2f6d983802dbc
charanrhel <charan.rhel@gmail.com>
2022-09-28 10:40:34 -0700

    update nodes from t3 to t4g in eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -21,7 +21,7 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t4g.small"]
 
       min_size     = 1
       max_size     = 3
@@ -39,7 +39,7 @@ module "eks" {
     two = {
       name = "node-group-2"
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t4g.small"]
 
       min_size     = 1
       max_size     = 2
